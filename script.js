f2dbf5f34f00bb7d1cc0f27184ad6a4d1a96d573
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-11-20 16:26:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let input1=prompt("First Input:");
+let input2=prompt("Second Input:");
+
+let num1=parseFloat(input1);
+let num2=parseFloat(input2)
+
+let result= (num1*2)+num2;
+alert('The final result is: ' + result);
